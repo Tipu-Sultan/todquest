3120ff9769765df53188b54549aacff5a4f6863b
Tipu Sultan <tipu@student.iul.ac.in>
2024-07-11 21:24:05 +0530

    fix images

--- todquest/src/components/LoginPage.jsx
+++ todquest/src/components/LoginPage.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import './LoginPage.css';
-import img from './loginPage.jpg';
+import img from './loginPage.png';
 
 const LoginPage = () => {
   return (
-    <div className="login-container">
-      <div className="login-left">
-        <div className="login-image">
-          <img src={img} alt="Login Illustration" />
+    <div className="login-container" style={{ backgroundImage: `url(${img})` }}>
+      <div className="login-box">
+        <div className="login-header">
+          <h2>ALREADY MEMBERS</h2>
+          <a href="/help" className="help-link">Need help?</a>
         </div>
-      </div>
-      <div className="login-right">
-        <div className="login-box">
-          <div className="login-header">
-            <h4>ALREADY MEMBERS</h4>
-            <a href="/help" className="help-link">Need help?</a>
+        <form>
+          <div className="input-group">
+            <input type="text" placeholder="Mahisa Dyan Diptya" />
           </div>
-          <form>
-            <div className="input-group">
-              <input type="text" placeholder="Mahisa Dyan Diptya" />
-            </div>
-            <div className="input-group">
-              <input type="password" placeholder="Enter your password" />
-            </div>
-            <button type="submit">SIGN IN</button>
-          </form>
-          <div className="login-footer">
-            <p>Don't have an account yet? <a href="/register">Create an account</a></p>
+          <div className="input-group">
+            <input type="password" placeholder="Enter your password" />
           </div>
+          <button type="submit">SIGN IN</button>
+        </form>
+        <div className="login-footer">
+          <p>Don't have an account yet? <a href="/register">Create an account</a></p>
         </div>
       </div>
     </div>
